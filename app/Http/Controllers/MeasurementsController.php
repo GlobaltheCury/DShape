593993b1e01998c90d2dbeece6d00196a0068a43
Giulliano Cury <giullianocury@gmail.com>
2022-06-08 14:27:58 -0300

    adicionando banco de dados na lista

---
+++ app/Http/Controllers/MeasurementsController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Measurements;
+use Illuminate\Http\Request;
+
+class MeasurementsController extends Controller
+{
+    
+    public function index (){
+        $measurements = Measurements::all();
+        return view('/measurements/list', ['measurements'=> $measurements]);
+    }
+
+    public function store(Request $request){
+        //criando o objeto Medidas
+        $measurements = new Measurements();
+
+        //alterando os atributos do objeto
+        $measurements->date = $request-> date;
+        $measurements->weight = $request -> weight;
+        $measurements->height = $request -> height;
+        $measurements->chest = $request -> chest;
+        $measurements->left_arm = $request -> left_arm;
+        $measurements->right_arm = $request-> right_arm;
+        $measurements->abdomen = $request-> abdomen;
+        $measurements->waist = $request -> waist;
+        $measurements->hips = $request -> hips;
+        $measurements->left_thigh = $request -> left_thigh;
+        $measurements->right_thigh = $request -> right_thigh;
+        $measurements->left_calf = $request -> left_calf;
+        $measurements->right_calf = $request -> right_calf;
+
+
+        //Encaminha para salvar no bd
+        $measurements->save();
+
+        //Redirecionar para a pagina de lista de medidas
+        return redirect('/measurements');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\MeasurementsController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('measurements/home');
+});
+
+Route::get('/measurements', [MeasurementsController::class, 'index'] );
+
+Route::get('/measurements/new', function () {
+    return view('measurements/form');
+});
+
+Route::post('/measurements/new', [MeasurementsController::class, 'store']);
