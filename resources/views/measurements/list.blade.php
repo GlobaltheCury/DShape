11bc38bbd1f2b63ffd0be5657bb0af26db5e9ecd
Giulliano Cury <giullianocury@gmail.com>
2022-08-10 14:32:41 -0300

    master

--- resources/views/measurements/list.blade.php
+++ resources/views/measurements/list.blade.php
@@ -33,9 +33,9 @@
                     <td>{{$measurement->date}}</td>
                     <td>
                       <button class="btn btn-secondary">Editar</button>
-                      <button class="btn btn-danger">Excluir</button>
-                      <button id="botao"  type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-teste="Ola Javascript!">
-                        Launch demo modal
+                      
+                      <button  type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal" data-teste="{{$measurement->id}}">
+                       Excluir
                       </button>
 
                     </td>
@@ -57,25 +57,37 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+        <h5 class="modal-title" id="exampleModalLabel">Atenção!</h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
+        Deseja Realmente excluir essa medida?
         <p id="paragrafo" ></p>
       </div>
       <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+        
+        <form action="" method="" id="">
+          <button type="button" class="btn btn-primary">Sim</button>
+
+        </form>
       </div>
     </div>
   </div>
 </div>
 
 <script>
-  let botao = document.getElementById('botao')
+let confirmationModal = document.getElementById('exampleModal')
+
+confirmationModal.addEventListener('show.bs.modal' , function (event) {
+
+  let botao = event.relatedTarget
   let paragrafo = document.getElementById('paragrafo')
 
   paragrafo.innerHTML = botao.getAttribute('data-teste')
+});
+
+
 </script>
 
 
